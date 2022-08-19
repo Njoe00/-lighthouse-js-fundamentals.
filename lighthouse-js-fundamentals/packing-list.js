248108f3d52f0ee3b5607eb31f630a1031cdf253
Njoe00 <nick.james.joe@gmail.com>
2022-08-18 21:17:39 -0700

    using arrays to create packing lists for moving

---
+++ lighthouse-js-fundamentals/packing-list.js
@@ -0,0 +1,6 @@
+const packingList = ["bowls", "plates","pots", "pans", "eating untenils", "glasses", "cups", "cooking untenils"]
+
+console.log("kitchen stuff to pack:");
+for (let i = 0; i < packingList.length; i++) {
+    console.log(packingList[i]);
+}
